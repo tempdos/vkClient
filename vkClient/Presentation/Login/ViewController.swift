68172205288d98705017d7d952fdd9099e661e44
tempdos <tempdos@gmail.com>
2021-07-09 02:58:39 +0900

    small fix

--- vkClient/Presentation/Login/ViewController.swift
+++ vkClient/Presentation/Login/ViewController.swift
@@ -9,11 +9,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setViews()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Подписываемся на уведомления: одно приходит при появлении клавиатуры
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+        // Второе когда клавиатура пропадает
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        // Отписываемся от уведомлений о клавиатуре
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        // Получаем текст логина и пароля
+        guard
+            let login = loginTextField.text,
+            let password = passwordTextField.text
+        else {
+            print("Login or password empty")
+            return
+        }
+        
+        // Проверяем, верны ли они
+        if login == "admin" && password == "12345" {
+            print("Успешная авторизация")
+        } else {
+            print("Не успешная авторизация")
+        }
+    }
+    
+    // Когда клавиатура появляется
+    @objc private func keyboardWasShown(notification: Notification) {
+        // Получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
 
+        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+
+    // Когда клавиатура исчезает
+    @objc private func keyboardWillBeHidden(notification: Notification) {
+        // Устанавливаем отступ внизу UIScrollView
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc private func hideKeyboard() {
+        self.scrollView?.endEditing(true)
+    }
+
+    private func setViews() {
+        loginButton.layer.cornerRadius = 8
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        scrollView.addGestureRecognizer(tapGesture)
+    }
 }
 
