bc3bc9a0034d642a9e3c984b87bbd8f8d1f412bf
tempdos <tempdos@gmail.com>
2021-07-09 01:57:57 +0900

    Lesson 1

---
+++ vkClient/Presentation/Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vkClient
+//
+//  Created by Василий Слепцов on 09.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
